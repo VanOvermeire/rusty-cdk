510e99ce2264be4fb8575c1fd36d4247e63ee9ac
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2025-11-20 20:50:11 +0100

    Fix another todo

--- cloud-infra-core/src/iam/permission_mapper.rs
+++ cloud-infra-core/src/iam/permission_mapper.rs
@@ -30,9 +30,10 @@ pub(crate) fn map_toml_dependencies_to_services(file_path: &Path) -> Vec<String>
 fn map_string_of_tom_dependencies_to_aws_services(file_content: &str) -> Vec<String> {
     let services: Vec<_> = AWS_SERVICES_LIST.split("\n").collect();
     let dependencies = file_content.split("\n").skip_while(|v| !v.contains("[dependencies]"));
-    // TODO stop once we reach the next section (optionally could scan dev-deps)
 
     dependencies
+        .skip(1) // skip [dependencies]
+        .take_while(|v| !v.starts_with("["))
         .filter(|v| v.contains("aws-sdk"))
         .map(|v| {
             let dependency_and_version: Vec<_> = v.split("=").collect();
@@ -48,11 +49,20 @@ mod tests {
     use crate::iam::permission_mapper::map_string_of_tom_dependencies_to_aws_services;
 
     #[test]
-    fn should() {
+    fn should_find_services_mentioned_in_dependencies() {
         let services = map_string_of_tom_dependencies_to_aws_services(
-            "[package]\nname = \"a-name\"\nversion = \"0.1.0\"\nedition = \"2024\"\n\n[dependencies]\ntokio = { version = \"1\", features = [\"full\"] }\nserde = { version = \"1.0.219\", features = [\"serde_derive\", \"derive\"] }\nserde_json = \"1.0.142\"\naws-config = { version = \"1.1.7\", features = [\"behavior-version-latest\"] }\naws-sdk-cloudformation = \"1.90.0\"\naws-sdk-s3 = \"1.103.0\"\n\n[dev-dependencies]\ninsta = {  version = \"1.43.1\", features = [\"json\", \"filters\"] }\nserde_json = \"1.0.142\"",
+            "[package]\nname = \"a-name\"\nversion = \"0.1.0\"\nedition = \"2024\"\n\n[dependencies]\ntokio = { version = \"1\", features = [\"full\"] }\nserde = { version = \"1.0.219\", features = [\"serde_derive\", \"derive\"] }\nserde_json = \"1.0.142\"\naws-config = { version = \"1.1.7\", features = [\"behavior-version-latest\"] }\naws-sdk-cloudformation = \"1.90.0\"\naws-sdk-s3 = \"1.103.0\"\n\n",
         );
         
         assert_eq!(services, vec!["cloudformation".to_string(), "s3".to_string()])
+    }
+
+    #[test]
+    fn should_find_services_mentioned_in_dependencies_ignoring_dev_deps() {
+        let services = map_string_of_tom_dependencies_to_aws_services(
+            "[package]\nname = \"a-name\"\nversion = \"0.1.0\"\nedition = \"2024\"\n\n[dependencies]\ntokio = { version = \"1\", features = [\"full\"] }\nserde = { version = \"1.0.219\", features = [\"serde_derive\", \"derive\"] }\nserde_json = \"1.0.142\"\naws-config = { version = \"1.1.7\", features = [\"behavior-version-latest\"] }\naws-sdk-s3 = \"1.103.0\"\n\n[dev-dependencies]\ninsta = {  version = \"1.43.1\", features = [\"json\", \"filters\"] }\naws-sdk-cloudformation = \"1.90.0\"\nserde_json = \"1.0.142\"",
+        );
+        
+        assert_eq!(services, vec!["s3".to_string()])
     }
 }
