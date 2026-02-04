05bd3543126942073e1e4884800a14b74ce22fb2
Sam Van Overmeire <10529492+VanOvermeire@users.noreply.github.com>
2026-02-04 17:42:42 +0100

    Make retrieve resources check for additional links to follow

--- resources-scraper/src/retrieve_resources/main.rs
+++ resources-scraper/src/retrieve_resources/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Ok, Result};
+use regex::Regex;
 use reqwest::blocking::Client;
 use reqwest::header::USER_AGENT;
 use scraper::selectable::Selectable;
@@ -6,8 +7,15 @@ use scraper::{Html, Selector};
 use std::collections::HashMap;
 use std::fs;
 use std::fs::read_to_string;
+use std::sync::OnceLock;
+
+static CUSTOM_PROP_TYPE_REGEX: OnceLock<Regex> = OnceLock::new();
 
 const BASE_URL: &str = "https://docs.aws.amazon.com/AWSCloudFormation/latest/TemplateReference";
+const TYPE_INFO: &str = "Type";
+// which should be the last bit of info for a given prop
+const UPDATE_REQUIRES_INFORMATION: &str = "Update";
+
 
 /// Retrieves raw, but parsable (CSV), resource info from the AWS docs
 /// Output is written to the output dir
@@ -19,20 +27,27 @@ fn main() -> Result<()> {
     
     let input = read_to_string("./output/list_of_urls")?;
     let resource_urls = input.split("\n").filter(|v| !v.is_empty());
+    let mut sublinks_to_check = vec![];
 
     for url in resource_urls {
         let resources = get_specific_resource(&client, url)?;
     
         for r in resources {
-            let (name, props) = retrieve_resource_props(&client, &r)?;
-            let joined_props = props
-                .into_iter()
-                .map(|v| format!("{}==={}", v.0, v.1.join("###")))
-                .collect::<Vec<_>>()
-                .join(";");
-            let output_for_single_resource = format!("{};{}", name, joined_props);
-            output.push(output_for_single_resource);
+            let (name, props, mut additional_links) = retrieve_resource_props(&client, &r)?;
+            output.push(output_for_single_resource(&name, &props));
+            sublinks_to_check.append(&mut additional_links);
         }
+    }
+    
+    while sublinks_to_check.len() > 0 {
+        println!("Checking {} additional links", sublinks_to_check.len());
+        let mut new_sublinks = vec![];
+        for url in sublinks_to_check {
+            let (name, props, mut additional_links) = retrieve_resource_props(&client, &url)?;
+            output.push(output_for_single_resource(&name, &props));
+            new_sublinks.append(&mut additional_links);
+        }
+        sublinks_to_check = new_sublinks;
     }
 
     fs::write("output/raw_resources.csv", output.join("\n").as_bytes())?;
@@ -40,7 +55,16 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn retrieve_resource_props(client: &Client, path: &str) -> Result<(String, HashMap<String, Vec<String>>)> {
+fn output_for_single_resource(name: &str, props: &HashMap<String, Vec<String>>) -> String {
+    let joined_props = props
+        .into_iter()
+        .map(|v| format!("{}==={}", v.0, v.1.join("###")))
+        .collect::<Vec<_>>()
+        .join(";");
+    format!("{};{}", name, joined_props)
+}
+
+fn retrieve_resource_props(client: &Client, path: &str) -> Result<(String, HashMap<String, Vec<String>>, Vec<String>)> {
     let resource_url = format!("{}/{}", BASE_URL, path);
     println!("Retrieving props for {}", resource_url);
     let resource_html = client
@@ -65,6 +89,7 @@ fn retrieve_resource_props(client: &Client, path: &str) -> Result<(String, HashM
     let prop_name_selector = Selector::parse("dt > span > code").unwrap();
     let mut prop_names = variable_list.select(&prop_name_selector);
 
+    let mut additional_links = vec![];
     let mut names = vec![];
 
     while let Some(name) = prop_names.next() {
@@ -79,21 +104,32 @@ fn retrieve_resource_props(client: &Client, path: &str) -> Result<(String, HashM
     let mut current_name = names_iter.next();
     let mut map: HashMap<String, Vec<String>> = HashMap::new();
     let mut gather_info = vec![];
+    
+    let custom_prop_type_regex = CUSTOM_PROP_TYPE_REGEX.get_or_init(|| Regex::new(r#".*<a href=\"(?P<url>.+?)\">.+</a>"#).unwrap());
 
     while let Some(info) = prop_info.next() {
         let name = info.inner_html();
         let name_without_em = name.replace("<em>", "").replace("</em>", "").replace("\n", "");
 
-        if name_without_em.starts_with("Update") {
+        if name_without_em.starts_with(UPDATE_REQUIRES_INFORMATION) {
             // this also means we don't add the Update info (currently not needed)
             map.insert(current_name.take().context("should be a prop name for every collection of info")?, gather_info.drain(..).collect());
             current_name = names_iter.next();
         } else {
+            if name_without_em.starts_with(TYPE_INFO) {
+                if let Some(m) = custom_prop_type_regex.captures(&name_without_em) {
+                    if m["url"].starts_with("https://") {
+                        println!("Ignoring external link: {}", &m["url"]);
+                    } else {
+                        additional_links.push(m["url"].replace("./", ""));                        
+                    }
+                }
+            }
             gather_info.push(name_without_em);
         }
     }
 
-    Ok((name, map))
+    Ok((name, map, additional_links))
 }
 
 fn get_specific_resource(client: &Client, suffix: &str) -> Result<Vec<String>> {
@@ -116,8 +152,13 @@ fn get_specific_resource(client: &Client, suffix: &str) -> Result<Vec<String>> {
 
     while let Some(el) = main.next() {
         let href = el.attr("href").context("a element should have href")?.to_string();
-        let href = href.replace("./", "");
-        resources.push(href);
+
+        if href.starts_with("https://") {
+            println!("Ignoring external link: {}", href);
+        } else {
+            let href = href.replace("./", "");
+            resources.push(href);
+        }
     }
 
     Ok(resources)
